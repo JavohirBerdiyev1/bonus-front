0e1caeff1ee01551340c4f259c119cc31355f26a
Бердиев Джавохир <j.berdiyev@trustbank.uz>
2025-05-15 17:55:01 +0500

    logo change

--- src/views/auth/SignIn/SignInForm.js
+++ src/views/auth/SignIn/SignInForm.js
@@ -30,7 +30,7 @@ const SignInForm = () => {
 			setSubmitting(true)
 			const hashedPassword = sha1(values.password).toString();
       const response = await fetch(
-        `${import.meta.env.VITE_API_BASE_URL}/api/v1/auth/login`,
+        `http://localhost:3000/api/v1/auth/login`,
         {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
